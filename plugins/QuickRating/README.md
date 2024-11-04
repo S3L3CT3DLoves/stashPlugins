789218593fb500890a708fd84e80ec1453670d24
S3L3CT3DLoves <153332410+S3L3CT3DLoves@users.noreply.github.com>
2024-11-04 11:51:43 +0100

    New plugin, QuickRating!

---
+++ plugins/QuickRating/README.md
@@ -0,0 +1,24 @@
+# *QuickRating*
+Replaces the rating input in Stash to make it easier to select a scene's rating with one click
+
+# Requirement
+- Stash (v0.27+)
+- Install CommunityScriptsUILibrary from the Stash Community repo
+
+# Goal
+For people who want a score with more granularity than the 5 stars in Stash.
+
+The *half-star* rating in Stash is too fiddly and requires too many clicks to get the correct rating.
+
+The *decimal* rating is precise, but forces you to use the keyboard + mouse.
+
+The goal with this plugin is to give more options to the user, that can be used one-handed, and in a single click.
+
+# Usage
+After installing it in your Stash, enable one of the configuration flags in the Stash plugin page.
+- Slide 10: Will display the score as a slider between 1 and 10, with a small textbox to display the number
+- Slide 20: Will display the score as a slider between 1 and 20, with a small textbox to display the number
+
+Not enabling any of the configuration flags will simply use the native rating system in Stash.
+
+*More modes to come, I hope to add a 1-10 star score soon.*
---
+++ plugins/QuickRating/quickRating.js
@@ -0,0 +1,139 @@
+const ratingSliderHTML = `
+<div class="input-group">
+    <input placeholder="Rating" name="rating" type="range" id="rating-slider" class="form-range form-control" min="1" max="10" value="">
+    <span class="input-group-text" id="rating-output">0</span>
+</div>
+`
+
+const scenePagePattern = new RegExp("/scenes/(\\d+)")
+let quickRatingConfig = {}
+
+class QuickRating{
+  constructor(){
+    this.mode = ""
+    this.maxValue = 1
+    // Init based on plugin config
+    if (quickRatingConfig["Astar10"]){
+      this.mode == "STAR"
+      this.maxValue = 10
+    }
+    else if (quickRatingConfig["Bslide10"]){
+      this.mode = "SLIDE"
+      this.maxValue = 10
+    }
+    else if (quickRatingConfig["Cslide20"]){
+      this.mode = "SLIDE"
+      this.maxValue = 20
+    }
+    else{
+      this.mode = "NATIVE"
+      this.maxValue = 5
+    }
+
+    this.ratingSliderElement = null
+    this.ratingDisplayElement = null
+    this.rating = 0
+    this.sceneId = window.location.pathname.split('/')[2]
+    this.getRatingFromAPI()
+  }
+
+  updateDisplay(){
+    const ratingContainer = document.querySelector(".scene-toolbar-group > div")
+    switch (this.mode) {
+      case "STAR":
+        // Not supported yet
+        break;
+      case "SLIDE":
+        ratingContainer.innerHTML = ratingSliderHTML
+        this.ratingSliderElement = document.getElementById("rating-slider")
+        this.ratingDisplayElement = document.getElementById("rating-output")
+
+        this.ratingSliderElement.max = this.maxValue
+        this.ratingSliderElement.addEventListener("input", (e) => { this.changeValue(e) })
+        console.log(this.ratingSliderElement)
+        this.displayValue()
+        break;
+      default:
+        // Use native
+        break;
+    }
+  }
+
+  async getRatingFromAPI(){
+    const query = `
+          query FindScene($id: ID!) {
+            findScene(id: $id) {
+              id
+              title
+              rating100
+            }
+          }`;
+    const variables = { id: this.sceneId };
+    const response = await csLib.callGQL({ query, variables });
+    let rating100 = response["findScene"]["rating100"] ?? 0
+    this.rating = rating100 / (100 / this.maxValue)
+  }
+
+  async updateRatingAPI(newRating){
+    const queryBody = {
+      operationName : "SceneUpdate",
+      query : `
+        mutation SceneUpdate($input: SceneUpdateInput!) {
+            sceneUpdate(input: $input) {
+              rating100
+            }
+          }`,
+      variables :{
+          input : {
+              id : this.sceneId,
+              rating100 : newRating * (100 / this.maxValue)
+          }
+      }
+  }
+    await csLib.callGQL(queryBody);
+  }
+
+  displayValue(){
+    this.ratingSliderElement.value = this.rating
+    this.ratingDisplayElement.innerText = this.rating
+  }
+
+  changeValue(e){
+    this.rating = e.target.value
+    this.updateRatingAPI(e.target.value)
+    this.displayValue()
+  }
+}
+
+async function quickRating(page){
+  if(Object.keys(quickRatingConfig).length == 0){
+    // Init plugin
+    console.log("Init quickRating")
+    quickRatingConfig = await getConfig()
+  }
+
+  if (scenePagePattern.test(page)){
+    let quickRatingPlugin = new QuickRating()
+    csLib.waitForElement(".scene-toolbar-group", () => {
+      quickRatingPlugin.updateDisplay()
+    })
+  }
+}
+
+async function getConfig(){
+  const defaultQuickRatingConfig = {
+    "native": true,
+    "Astar10": false,
+    "Bslide10": false,
+    "Cslide20": false
+  }
+  const config = await csLib.getConfiguration('quickRating', defaultQuickRatingConfig)
+  return {...defaultQuickRatingConfig, ...config}
+}
+
+
+PluginApi.Event.addEventListener("stash:location", (e) => {
+    quickRating(e.detail.data.location.pathname)
+});
+
+quickRating(window.location.pathname)
